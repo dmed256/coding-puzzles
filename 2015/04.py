681ce6d5a0e424faa27b0f54ad0c52386ca8fb85
dmed256 <dmed256@gmail.com>
2021-10-28 19:23:12 -0500

    [2015] Added 04

---
+++ 2015/04.py
@@ -0,0 +1,19 @@
+import hashlib
+from advent_of_code import *
+
+def md5(s):
+    return hashlib.md5(str.encode(s)).hexdigest()
+
+@testable
+def run(s, hash_prefix):
+    suffix = 0
+    while not md5(f'{s}{suffix}').startswith(hash_prefix):
+        suffix += 1
+    return suffix
+
+# run('abcdef', '00000').should_be(609043)
+# run('pqrstuv', '00000').should_be(1048970)
+
+# run('iwrupvqb', '00000').debug('Star 1')
+
+run('iwrupvqb', '000000').debug('Star 2')
