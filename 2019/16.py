95a5dce2785aa83a85792db68716abafbecc2b34
dmed256 <dmed256@gmail.com>
2021-11-01 21:39:40 -0500

    [2019] 16

--- 2019/16.py
+++ 2019/16.py
@@ -68,19 +68,17 @@ def apply_blocked_fft(blocks, block, value, digits):
     return output
 
 def apply_fft(value, digits):
-    outputs = pmap('fft', apply_blocked_fft, [value, digits])
-    output = np.sum(outputs, axis=0).astype(int)
+    if True:
+        output = apply_blocked_fft(1, 0, value, digits)
+    else:
+        outputs = pmap('fft', apply_blocked_fft, [value, digits])
+        output = np.sum(outputs, axis=0).astype(int)
 
     # Get the first digit only
     for i in range(digits):
         output[i] = abs(output[i]) % 10
 
     return output
-
-def apply_fast_fft(value, digits):
-
-
-
 
 def get_subdigit(value, offset, count):
     subdigit = 0
@@ -89,48 +87,49 @@ def get_subdigit(value, offset, count):
         subdigit += value[i]
     return subdigit
 
-def run(value, problem):
+def run(value):
     value = [int(c) for c in value]
-    if problem == 2:
-        value = value * 10000
     digits = len(value)
 
     for i in range(100):
-        if problem == 2:
-            tic()
-
         value = apply_fft(value, digits)
 
-        if problem == 2:
-            toc(f'FFT phase = {i}')
-
-    if problem == 1:
-        return get_subdigit(value, 0, 8)
-
-    offset = get_subdigit(value, 0, 7)
-    return get_subdigit(value, offset, 8)
+    return get_subdigit(value, 0, 8)
 
 example1 = '80871224585914546619083218645595'
 example2 = '19617804207202209144916044189917'
 example3 = '69317163492948606335995924319873'
 
-run(example1, 1) | eq(24176176)
-run(example2, 1) | eq(73745418)
-run(example3, 1) | eq(52432133)
+run(example1) | eq(24176176)
+run(example2) | eq(73745418)
+run(example3) | eq(52432133)
 
 input_value = get_input()
 
-run(input_value, 1) | debug('Star 1')
+run(input_value) | debug('Star 1')
 
-example1 = '03036732577212944063491565474664'
-example2 = '02935109699940807407585447034323'
-example3 = '03081770884921959731165446850517'
+def run2(value):
+    value = [int(c) for c in value]
+    offset = get_subdigit(value, 0, 7)
 
-print('example1')
-run(example1, 2) | eq(84462026)
-# print('example2')
-# run(example2, 2) | eq(78725270)
-# print('example3')
-# run(example3, 2) | eq(53553731)
+    value = value * 10000
+    value = value[offset:]
+    digits = len(value)
 
-# run(input_value, 2) | debug('Star 2')
+    # Apply top-right 1 matrix
+    #
+    # 1 1 1 1 1     v1
+    # . 1 1 1 1     v2
+    # . . 1 1 1  x  v3
+    # . . . 1 1     v4
+    # . . . . 1     v5
+
+    for loop in range(100):
+        for i in range(1, digits):
+            idx = digits - i - 1
+            value[idx] += value[idx + 1]
+            value[idx] %= 10
+
+    return get_subdigit(value, 0, 8)
+
+run2(input_value) | debug('Star 2')
