4aa5ba8b3eae5d7892702c0039415c462f339280
dmed256 <dmed256@gmail.com>
2021-11-06 21:40:58 -0500

    [2015] 14.0

---
+++ 2015/14.py
@@ -0,0 +1,14 @@
+from advent_of_code import *
+
+def run(lines):
+    pass
+
+example1 = multiline_lines("""
+""")
+
+run(example1) | eq()
+
+input_value = get_input()
+input_lines = get_input_lines()
+
+run(input_) | debug('Star 1')
