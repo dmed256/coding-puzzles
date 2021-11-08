616f7a478c8cd7919c8fd4cb1fa9c75716fbbe45
dmed256 <dmed256@gmail.com>
2021-11-07 19:07:28 -0600

    [2017] 11

---
+++ template.py
@@ -0,0 +1,11 @@
+from advent_of_code import *
+
+example = multiline_lines(r"""
+""")
+
+run() | eq()
+
+input_value = get_input()
+input_lines = get_input_lines()
+
+run(input_lines) | debug('Star 1')
