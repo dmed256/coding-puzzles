afe7658f257ef48bfac0b36ee74af3f197e5935f
dmed256 <dmed256@gmail.com>
2021-11-07 23:51:51 -0600

    [2019] Clean up 21

--- 2019/21.py
+++ 2019/21.py
@@ -2,29 +2,24 @@ from advent_of_code import *
 from int_processor import *
 
 class Problem:
-    def __init__(self):
+    def __init__(self, solution):
         self.ascii_output = ''
-        self.ascii_input = ''
+        self.ascii_input = [
+            c
+            for line in solution.splitlines()
+            if line and not line.startswith('#')
+            for c in (line + '\n')
+        ]
+        self.hull_damage = 0
 
     def get_input(self):
-        while not self.ascii_input:
-            if self.ascii_output:
-                print(f'> {self.ascii_output}')
-                self.ascii_output = ''
-
-            ascii_input = input() + '\n'
-            if not ascii_input.strip() or ascii_input.startswith('#'):
-                continue
-
-            self.ascii_input = [c for c in ascii_input]
-
         return ord(self.ascii_input.pop(0))
 
     def process_output(self, output):
         [code] = output
 
         if code >= 256:
-            print(f'Code = {code}')
+            self.hull_damage = code
             return output
 
         self.ascii_output += chr(code)
@@ -38,70 +33,34 @@ class Problem:
         p.process_output = self.process_output
         p.run()
 
-        return self.ascii_output
+        return self.hull_damage
 
-# Star 1
-"""
+solution1 = multiline_input("""
 # If there's no floor in the next time, JUMP!
 NOT A J
+
 # Jump if there is a space to jump
 OR D J
+
 # Check if all 4 tiles have floor, if they do DON'T JUMP YET!
 OR A T
 AND B T
 AND C T
 AND D T
+
 # !(DON'T JUMP) -> Maybe jump...
 NOT T T
 AND T J
 WALK
-"""
+""")
 
-# Star 2
-"""
-@ _ _ . _ . _ . _ .
-0 1 2 3 4 5 6 7 8 9
-  A B C D E F G H I
-
-@ x _ _ x _
-0 5 6 7 8 9
-  E F G H I
-
-. . . . . . . . . . . . . . . . .
-. . . . . . . . . . . . . . . . .
-. . . . . . . . . . . . . . . . .
-# # # # # . # . # . # # . . # # #
-  A B C D E F G H I
-
-. . . . . . . . . . . . . . .
-. . . . . . . . . . . . . . .
-. . . . . . . . . . . . . . .
-J # # . J . # . # # . . # # #
-  A B C D E F G H I
-
-. . . . . . . . . . . . . . . . .
-. . . . . . . . . . . . . . . . .
-. . . . . . . . . . . . . . . . .
-# # # # # . # # # . # . . . # # #
-  A B C D E F G H I
-
-. . . . . . . . . . . . . . .
-. . . . . . . . . . . . . . .
-. . . . . . . . . . . . . . .
-J # # . J # # . J . . . # # #
-  A B C D E F G H I
-
-
-if not A:
-    return True
-if D & (H | (E & (I OR F))) & !(B & C & D)
-    return True
-
-
-!(B & F & !H)
-((!B || !F) || H)
-(!(B && F) || H)
-
+solution2 = multiline_input("""
+# if D & (H | (E & (I OR F))) & !(B & C & D)
+#     return True#
+#
+# !(B & F & !H)
+# ((!B || !F) || H)
+# (!(B && F) || H)
 OR I T
 OR F T
 AND E T
@@ -109,6 +68,7 @@ OR H T
 AND D T
 OR T J
 
+# !(B & C & D)
 NOT B T
 NOT T T
 AND C T
@@ -116,10 +76,14 @@ AND D T
 NOT T T
 AND T J
 
+# if not A:
+#     return True
 NOT A T
 OR T J
 
 RUN
-"""
+""")
 
-Problem().run() | debug()
+Problem(solution1).run() | debug('Star 1') | eq(19354083)
+
+Problem(solution2).run() | debug('Star 2') | eq(1143499964)
