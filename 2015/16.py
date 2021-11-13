9a85433ec05c4850258f6c4a561d2dadac3f55db
dmed256 <dmed256@gmail.com>
2021-11-13 13:32:43 -0600

    [2015] 16

--- 2015/16.py
+++ 2015/16.py
@@ -1,18 +1,60 @@
 from advent_of_code import *
 
-input_value = get_input()
 input_lines = get_input_lines()
 
 def run(lines):
-    pass
+    info = multiline_lines(r"""
+children: 3
+cats: 7
+samoyeds: 2
+pomeranians: 3
+akitas: 0
+vizslas: 0
+goldfish: 5
+trees: 3
+cars: 2
+perfumes: 1
+    """.strip())
+    info = {
+        words[0]: int(words[1])
+        for line in info
+        if (words := line.split(': '))
+    }
 
-example1 = multiline_lines(r"""
-""")
+    sue1 = 0
+    sue2 = 0
+    for i, line in enumerate(input_lines):
+        sue = i + 1
+        words = ', '.join(line.split(': ')[1:])
+        words = words.split(', ')
 
-run(example1) | eq()
+        item_count = len(words) // 2
+        items = [words[i] for i in range(0, len(words), 2)]
+        counts = [int(words[i]) for i in range(1, len(words), 2)]
 
-run(input_lines) | debug('Star 1')
+        valid_items1 = 0
+        valid_items2 = 0
+        for item, count in zip(items, counts):
+            expected_count = info[item]
+            valid_items1 += count == expected_count
+            if item in ('cats', 'trees'):
+                valid_items2 += count > expected_count
+            elif item in ('pomeranians', 'goldfish'):
+                valid_items2 += count < expected_count
+            else:
+                valid_items2 += count == expected_count
 
-# run2(example1) | eq()
+        if valid_items1 == item_count:
+            sue1 = sue
+        if valid_items2 == item_count:
+            sue2 = sue
 
-# run2(input_lines) | debug('Star 2')
+        if sue1 and sue2:
+            break
+
+    return (sue1, sue2)
+
+(sue1, sue2) = run(input_lines)
+
+sue1 | debug('Star 1') | eq(103)
+sue2 | debug('Star 2') | eq(405)
