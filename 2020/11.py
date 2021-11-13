77d072a083cee59dac97c426aebbeb0b3f1d7693
dmed256 <dmed256@gmail.com>
2021-11-13 00:17:17 -0600

    [2020] 11

---
+++ 2020/11.py
@@ -0,0 +1,91 @@
+from advent_of_code import *
+
+input_value = get_input()
+input_lines = get_input_lines()
+
+def run(lines, problem):
+    grid = Grid(lines)
+    seats = set([
+        (x, y)
+        for (x, y, v) in grid
+        if v == 'L'
+    ])
+    empty_seats = seats.copy()
+    filled_seats = set()
+
+    directions = [
+        (dx, dy)
+        for dy in [-1, 0, 1]
+        for dx in [-1, 0, 1]
+        if (dx, dy) != (0, 0)
+    ]
+
+    neighbors = {}
+    for pos in seats:
+        if problem == 1:
+            neighbors[pos] = [
+                pos2
+                for direction in directions
+                if (pos2 := grid.apply_direction(pos, direction))
+                and pos2 in seats
+            ]
+        else:
+            neighbors[pos] = []
+            for direction in directions:
+                pos2 = pos
+                while pos2 := grid.apply_direction(pos2, direction):
+                    if pos2 in seats:
+                        neighbors[pos].append(pos2)
+                        break
+
+    seats_to_empty = (
+        4
+        if problem == 1 else
+        5
+    )
+
+    def find_adj_filled(filled_seats, pos):
+        return len([
+            1
+            for neighbor in neighbors[pos]
+            if neighbor in filled_seats
+        ])
+
+    changed = True
+    while changed:
+        changed = False
+        filled_seats_copy = filled_seats.copy()
+        empty_seats_copy = empty_seats.copy()
+
+        for pos in empty_seats_copy:
+            if find_adj_filled(filled_seats_copy, pos) == 0:
+                changed = True
+                filled_seats.add(pos)
+                empty_seats.remove(pos)
+
+        for pos in filled_seats_copy:
+            if find_adj_filled(filled_seats_copy, pos) >= seats_to_empty:
+                changed = True
+                filled_seats.remove(pos)
+                empty_seats.add(pos)
+
+    return len(filled_seats)
+
+example1 = multiline_lines(r"""
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL
+""")
+
+run(example1, 1) | eq(37)
+run(input_lines, 1) | debug('Star 1') | eq(2299)
+
+run(example1, 2) | eq(26)
+run(input_lines, 2) | debug('Star 2') | eq(2047)
