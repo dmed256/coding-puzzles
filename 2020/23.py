1e7ffb2528bc4888d0609005d240e316836c1e5c
dmed256 <dmed256@gmail.com>
2021-12-05 18:13:30 -0600

    [2020] 23.2

--- 2020/23.py
+++ 2020/23.py
@@ -1,32 +1,56 @@
 from advent_of_code import *
 
+class Node(BaseModel):
+    next_node: int
+
 class Problem:
     def __init__(self, problem, labels, moves):
         self.problem = problem
         self.labels = [int(x) - 1 for x in labels]
         self.moves = moves
 
-    def move(self, labels):
-        count = len(labels)
-        taken = labels[1:4]
-        labels = labels[4:] + [labels[0]]
-
-        current = (labels[-1] + count - 1) % count
-        while current in taken:
-            current = (current + count - 1) % count
-
-        dest = labels.index(current)
-        return labels[:dest+1] + taken + labels[dest+1:]
+        if problem == 2:
+            self.labels += list(range(len(labels), 1000000))
 
     def run(self):
         labels = self.labels
+        count = len(labels)
+        nodes = {
+            labels[i]: Node(next_node=labels[(i + 1) % count])
+            for i in range(count)
+        }
+
+        current = labels[0]
         for i in range(self.moves):
-            labels = self.move(labels)
+            n1 = nodes[current].next_node
+            n2 = nodes[n1].next_node
+            n3 = nodes[n2].next_node
 
-        pos1 = labels.index(0)
-        labels = labels[pos1+1:] + labels[:pos1]
+            next_current = nodes[n3].next_node
+            nodes[current].next_node = next_current
 
-        return ''.join([str(x + 1) for x in labels])
+            taken = [n1, n2, n3]
+            dest = (current - 1) % count
+            while dest in taken:
+                dest = (dest - 1) % count
+
+            end_node = nodes[dest].next_node
+            nodes[dest].next_node = n1
+            nodes[n3].next_node = end_node
+
+            current = next_current
+
+        if self.problem == 1:
+            current = 0
+            output = ''
+            for i in range(count - 1):
+                current = nodes[current].next_node
+                output += str(current + 1)
+            return output
+
+        n1 = nodes[0].next_node
+        n2 = nodes[n1].next_node
+        return (n1 + 1) * (n2 + 1)
 
 def run(*args):
     return Problem(1, *args).run()
@@ -41,6 +65,5 @@ run(example1, 10) | eq('92658374')
 run(example1, 100) | eq('67384529')
 run(input1, 100) | debug('Star 1') | eq('82934675')
 
-# run2(example1) | eq()
-
-# run2(input_lines) | debug('Star 2') | clipboard()
+run2(example1, 10000000) | eq(149245887792)
+run2(input1, 10000000) | debug('Star 2') | clipboard
