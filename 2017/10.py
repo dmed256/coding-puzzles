419670588cf2ef970f8cd447cbf8abe4e8c05461
dmed256 <dmed256@gmail.com>
2021-12-06 17:50:34 -0600

    [2017] 14

--- 2017/10.py
+++ 2017/10.py
@@ -1,72 +1,18 @@
 from advent_of_code import *
+from utils import *
 
 input_value = get_input()
 input_lines = get_input_lines()
-
-def hash_values(values, lengths, rounds):
-    values = deepcopy(values)
-
-    ptr = 0
-    skip = -1
-    for _ in range(rounds):
-        for length in lengths:
-            skip += 1
-
-            if length > len(values):
-                continue
-
-            p1 = ptr
-            p2 = (ptr + length) % len(values)
-
-            ptr = (ptr + length + skip) % len(values)
-
-            if p1 < p2:
-                values[p1:p2] = reversed(values[p1:p2])
-            elif length:
-                start = values[:p2]
-                end = values[p1:]
-                chunk = (end + start)[::-1]
-                values[:p2] = chunk[len(end):]
-                values[p1:] = chunk[:len(end)]
-
-    return values
 
 def run(lines, chars):
     values = list(range(chars))
     lengths = [int(x) for x in lines[0].split(',')]
 
-    values = hash_values(values, lengths, 1)
+    values = knot_hash_values(values, lengths, 1)
     return values[0] * values[1]
 
 def run2(content):
-    if type(content) is list:
-        content = content[0]
-
-    values = list(range(256))
-    lengths = (
-        [ord(c) for c in content]
-        + [17, 31, 73, 47, 23]
-    )
-
-    values = hash_values(values, lengths, 64)
-
-    hashed_value = []
-    for i in range(0, 256, 16):
-        value = 0
-        for j in range(i, i + 16):
-            value ^= values[j]
-        hashed_value.append(value)
-
-    def hex_value(v):
-        h = hex(v)[2:]
-        if len(h) == 2:
-            return h
-        return '0' + h
-
-    return ''.join(
-        hex_value(c)
-        for c in hashed_value
-    )
+    return get_knot_hash(content)
 
 example1 = multiline_lines(r"""
 3,4,1,5
---
+++ 2017/14.py
@@ -0,0 +1,77 @@
+from advent_of_code import *
+from utils import *
+
+class Problem:
+    def __init__(self, problem, content):
+        self.problem = problem
+        self.content = content
+
+    @staticmethod
+    def get_hex_bits(c):
+        value = int(f'0x{c}', 0)
+
+        h = ''
+        for bit in range(4):
+            h += '1' if value & (1 << bit) else '0'
+
+        return h[::-1]
+
+    def find_region(self, pos, missing_squares):
+        group = {pos}
+        queue = [pos]
+        while queue:
+            pos = queue.pop()
+            (r, c) = pos
+            for direction in DIRECTIONS:
+                pos2 = apply_direction(pos, direction)
+                if pos2 in missing_squares:
+                    queue.append(pos2)
+                    group.add(pos2)
+                    missing_squares.remove(pos2)
+        return group
+
+    def run(self):
+        grid = []
+        for i in range(128):
+            knot_hash = get_knot_hash(f'{self.content}-{i}')
+            grid.append(
+                ''.join([
+                    self.get_hex_bits(c)
+                    for c in knot_hash
+                ])
+            )
+
+        squares = [
+            (r, c)
+            for r, row in enumerate(grid)
+            for c, char in enumerate(row)
+            if char == '1'
+        ]
+
+        if self.problem == 1:
+            return len(squares)
+
+        missing_squares = set(squares)
+        region_count = 0
+        while missing_squares:
+            pos = min(missing_squares)
+            missing_squares.remove(pos)
+
+            region = self.find_region(pos, missing_squares)
+            missing_squares -= region
+            region_count += 1
+
+        return region_count
+
+
+def run(*args):
+    return Problem(1, *args).run()
+
+def run2(*args):
+    return Problem(2, *args).run()
+
+run('flqrgnkx') | eq(8108)
+run('oundnydw') | debug('Star 1') | clipboard()
+
+run2('flqrgnkx') | eq(1242)
+run2('oundnydw') | debug('Star 2') | clipboard()
