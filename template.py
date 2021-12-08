64b4377cfb69e5268a0119d3c100e63604fbb4c3
dmed256 <dmed256@gmail.com>
2021-12-07 22:14:44 -0600

    [2017] 20.2

--- template.py
+++ template.py
@@ -3,26 +3,16 @@ from advent_of_code import *
 input_value = get_input()
 input_lines = get_input_lines()
 
-class Problem:
-    def __init__(self, problem, lines):
-        self.problem = problem
-
-    def run(self):
-        return None
-
-def run(*args):
-    return Problem(1, *args).run()
-
-def run2(*args):
-    return Problem(2, *args).run()
+def run(problem, lines):
+    return None
 
 example1 = multiline_lines(r"""
 """)
 
-run(example1) | eq()
+run(1, example1) | eq()
 
-run(input_lines) | debug('Star 1') | clipboard()
+run(1, input_lines) | debug('Star 1') | clipboard()
 
-# run2(example1) | eq()
+# run(2, example1) | eq()
 
-# run2(input_lines) | debug('Star 2') | clipboard()
+# run(2, input_lines) | debug('Star 2') | clipboard()
