e488780dd407245f7a92b1a292fbadc58dc33b60
dmed256 <dmed256@gmail.com>
2021-12-09 19:40:43 -0600

    [2017] 21

--- 2017/21.py
+++ 2017/21.py
@@ -3,110 +3,96 @@ from advent_of_code import *
 input_value = get_input()
 input_lines = get_input_lines()
 
-def run(problem, lines, iterations):
-    def parse(s):
-        return s.split('/')
+def split_image(img):
+    return [
+        [c for c in row]
+        for row in img.split('/')
+    ]
 
-    def stringify(img):
-        return '/'.join(img)
+def join_image(img):
+    return '/'.join([
+        ''.join(row)
+        for row in img
+    ])
 
-    def rotate(img):
-        img = parse(img)
-        size = len(img)
-        return stringify([
-            ''.join(row[i] for row in reversed(img))
-            for i in range(size)
-        ])
+def get_arrangements(img):
+    grid = split_image(img)
+    arrangements = []
+    for _ in range(2):
+        for rotation in range(4):
+            arrangements.append(grid)
+            grid = [
+                [row[c] for row in reversed(grid)]
+                for c in range(len(grid))
+            ]
+        grid = grid[::-1]
 
-    def flip(img):
-        img = parse(img)
-        return stringify(img[::-1])
+    return [
+        join_image(grid)
+        for grid in arrangements
+    ]
 
-    def get_arrangements(img):
-        arrangements = []
-        for flip_ in range(2):
-            for rotation in range(4):
-                arrangements.append(img)
-                img = rotate(img)
-            img = flip(img)
-        return arrangements
+def get_subgrid(grid, r, c, size):
+    return [
+        [
+            grid[ri][ci]
+            for ri in range(r, r + size)
+        ]
+        for ci in range(c, c + size)
+    ]
 
-    def print_grid(grid):
-        output = ''
-        for row in grid:
-            for img_row in range(size):
-                for img in row:
-                    output += parse(img)[img_row]
-                output += '\n'
-        print(output)
+def split(grid, transforms, size):
+    width = len(grid)
+    expanded_grid = []
+    for r in range(0, width, size):
+        big_row = None
+        for c in range(0, width, size):
+            subgrid = get_subgrid(grid, r, c, size)
+            subgrid = transforms[join_image(subgrid)]
+            subgrid = split_image(subgrid)
 
-    maps = {}
+            if big_row is None:
+                big_row = [[] for i in range(len(subgrid))]
+            for i, row in enumerate(subgrid):
+                big_row[i] += row
+
+        for row in big_row:
+            expanded_grid.append(row)
+
+    return expanded_grid
+
+def run(lines, iterations):
+    transforms = {}
     for line in lines:
-        [left, right] = line.split(' => ')
-
+        left, right = line.split(' => ')
         for arrangement in get_arrangements(left):
-            maps[arrangement] = right
+            transforms[arrangement] = right
 
-    def split(grid):
-        grid = [
-            [parse(img) for img in row]
-            for row in grid
-        ]
-        next_grid = []
-        for row in grid:
-            next_row1 = []
-            next_row2 = []
-            for img in row:
-                split_img = [
-                    img_row[2*r:2*r+2]
-                    for img_row in img
-                    for r in range(2)
-                ]
-                next_row1 += [
-                    stringify([split_img[0], split_img[2]]),
-                    stringify([split_img[1], split_img[3]]),
-                ]
-                next_row2 += [
-                    stringify([split_img[4], split_img[6]]),
-                    stringify([split_img[5], split_img[7]]),
-                ]
-            next_grid.append(next_row1)
-            next_grid.append(next_row2)
+    grid = [
+        [c for c in row]
+        for row in '.#./..#/###'.split('/')
+    ]
 
-        return next_grid
-
-
-    grid = [['.#./..#/###']]
-    size = 3
     for i in range(iterations):
-        grid = [
-            [maps[img] for img in row]
-            for row in grid
-        ]
-        # Check for % rather than 3 -> 2 -> 3 -> 2 -> 3 -> 2
-        if size == 3:
-            grid = split(grid)
-            size = 2
+        if len(grid) % 2 == 0:
+            grid = split(grid, transforms, 2)
         else:
-            size = 3
-        print_grid(grid)
+            grid = split(grid, transforms, 3)
 
-    count = 0
-    for row in grid:
-        for img in row:
-            count += len([c for c in img if c == '#'])
-    return count
+    return len([
+        1
+        for row in grid
+        for c in row
+        if c == '#'
+    ])
 
 example1 = multiline_lines(r"""
 ../.# => ##./#../...
 .#./..#/### => #..#/..../..../#..#
 """)
 
-run(1, example1, 2) | eq(12)
+run(example1, 2) | eq(12)
 
-# Too low: 110
-# run(1, input_lines, 5) | debug('Star 1') | clipboard()
+run(input_lines, 5) | debug('Star 1') | eq(208)
 
-# run(2, example1) | eq()
-
-# run(2, input_lines) | debug('Star 2') | clipboard()
+run(input_lines, 18) | debug('Star 2') | eq(2480380)
