05a83feccb2d9b74ef40bce2b0debb54435ba3f4
dmed256 <dmed256@gmail.com>
2021-12-11 20:23:36 -0600

    [LC] Update

--- leetcode/weekly_contests/270/p1.py
+++ leetcode/weekly_contests/270/p1.py
@@ -1,7 +1,3 @@
-from repo_utils import *
-
-#---[ Solution ]----------------------------------
-
 import itertools
 
 class Solution:
@@ -14,14 +10,3 @@ class Solution:
                 numbers.add(digits[0] * 100 + digits[1] * 10 + digits[2])
 
         return sorted(list(numbers))
-
-#=================================================
-
-def run(inputs):
-    return Solution().findEvenNumbers(inputs)
-
-run([2,1,3,0]) | eq([102,120,130,132,210,230,302,310,312,320])
-run([2,2,8,8,2]) | eq([222,228,282,288,822,828,882])
-run([3,7,5]) | eq([])
-run([0,2,0,0]) | eq([200])
-run([0,0,0]) | eq([])
--- leetcode/weekly_contests/270/p2.py
+++ leetcode/weekly_contests/270/p2.py
@@ -1,7 +1,3 @@
-from repo_utils import *
-
-#---[ Solution ]----------------------------------
-
 import math
 
 class ListNode:
@@ -23,28 +19,3 @@ class Solution:
 
         node1.next = node1.next.next
         return head
-
-#=================================================
-
-def run(inputs):
-    head = ListNode(inputs[0])
-    node = head
-    for val in inputs[1:]:
-        node.next = ListNode(val)
-        node = node.next
-
-    head2 = Solution().deleteMiddle(head)
-    if not head2:
-        return []
-
-    ans = [head2.val]
-    while head2.next:
-        head2 = head2.next
-        ans.append(head2.val)
-    return ans
-
-
-run([1,3,4,7,1,2,6]) | eq([1,3,4,1,2,6])
-run([1,2,3,4]) | eq([1,2,4])
-run([2,1]) | eq([2])
-run([2]) | eq([])
