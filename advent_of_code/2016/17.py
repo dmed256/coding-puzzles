92e39bf9b92b0f686ddf75955941d6035e961265
dmed256 <dmed256@gmail.com>
2021-12-12 04:19:10 -0600

    [2016] 17 (temp)

---
+++ advent_of_code/2016/17.py
@@ -0,0 +1,14 @@
+from repo_utils import *
+
+def run(problem, value):
+    return None
+
+# run(1, 'ihgpwlah') | eq ('DDRRRD')
+# run(1, 'kglvqrro') | eq ('DDUDRLRRUDRD')
+# run(1, 'ulqzkmiv') | eq ('DRURDRUDDLLDLUURRDULRLDUUDDDRR')
+
+# run(1, 'qtetzkpl') | debug('Star 1') | clipboard()
+
+# run(2, example1) | eq()
+
+# run(2, input_lines) | debug('Star 2') | clipboard()
