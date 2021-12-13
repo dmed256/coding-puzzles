70049cbec37a3374e18264162f71c1f474cc3672
dmed256 <dmed256@gmail.com>
2021-12-12 19:07:35 -0600

    [2016] 22.2 (temp)

--- advent_of_code/2016/22.py
+++ advent_of_code/2016/22.py
@@ -5,7 +5,7 @@ input_lines = get_input_lines()
 def run(problem, lines):
     nodes = {}
     for line in lines[2:]:
-        node, size, used, avail, perc = [x.strip() for x in line.split()]
+        node, size, used, avail, _perc = [x.strip() for x in line.split()]
 
         x, y = node.replace('/dev/grid/node-x', '').split('-y')
         x = int(x)
@@ -14,9 +14,8 @@ def run(problem, lines):
         size = int(size[:-1])
         used = int(used[:-1])
         avail = int(avail[:-1])
-        perc = int(perc[:-1])
 
-        nodes[(x, y)] = (size, used, avail, perc)
+        nodes[(x, y)] = (size, used, avail)
 
     min_x = min([x for x, y in nodes.keys()])
     max_x = max([x for x, y in nodes.keys()])
@@ -34,9 +33,9 @@ def run(problem, lines):
     infos = list(nodes.values())
     pairs = 0
     for i in range(len(infos)):
-        _, used1, avail1, _ = infos[i]
+        _, used1, avail1 = infos[i]
         for j in range(i + 1, len(infos)):
-            _, used2, avail2, _ = infos[j]
+            _, used2, avail2 = infos[j]
             if used1 and used1 < avail2:
                 pairs += 1
             if used2 and used2 < avail1:
