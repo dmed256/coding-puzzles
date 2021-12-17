ef85f91c9333cc86cb1ac2f0de5a60656c5ced22
dmed256 <dmed256@gmail.com>
2021-12-16 20:24:31 -0600

    [-O3] 2020.22

--- advent_of_code/2020/22.py
+++ advent_of_code/2020/22.py
@@ -2,7 +2,7 @@ from repo_utils import *
 
 input_lines = get_input_lines()
 
-def play_game(problem, p1, p2, game_cache):
+def play_game(problem, p1, p2):
     cache = set()
     while p1 and p2:
         winner = None
@@ -15,16 +15,31 @@ def play_game(problem, p1, p2, game_cache):
         c1 = p1.pop(0)
         c2 = p2.pop(0)
 
-        if (winner is None
-            and problem == 2
+        needs_subgame = (
+            problem == 2
+            and winner is None
             and len(p1) >= c1
-            and len(p2) >= c2):
-            winner, _ = play_game(
-                problem,
-                p1[:c1],
-                p2[:c2],
-                game_cache,
-            )
+            and len(p2) >= c2
+        )
+        if needs_subgame:
+            sub_p1 = p1[:c1]
+            sub_p2 = p2[:c2]
+            # This specific optimization is taken from subreddit wisdom
+            # I couldn't figure out the trick to speed this up :(
+            #
+            # max(sub_p1) > max(sub_p2)
+            # -> If there are no recusive games, P1 will always win
+            #    due to having the largest card
+            #
+            # max(sub_p1) > c1 + c2 - 2
+            # -> This won't cause any recursive games due to
+            #    len(p#) < max(sub_p1)
+            #
+            # This doesn't apply to P2 because repeats cause P1 to win
+            if max(sub_p1) > max(sub_p2) and max(sub_p1) > c1 + c2 - 2:
+                winner = 1
+            else:
+                winner, _ = play_game(problem, sub_p1, sub_p2)
 
         if winner is None:
             winner = 1 if c1 > c2 else 2
@@ -39,12 +54,6 @@ def play_game(problem, p1, p2, game_cache):
     else:
         winner = (2, p2)
 
-    game_key2 = (game_key[1], game_key[0])
-    winner2 = (1 + (winner[0] % 2), winner[1])
-
-    game_cache[game_key] = winner
-    game_cache[game_key2] = winner2
-
     return winner
 
 
@@ -58,11 +67,15 @@ def run(problem, lines):
         else:
             players[-1].append(int(line))
 
-    _, deck = play_game(problem, players[0], players[1], {})
+    _, deck = play_game(
+        problem,
+        players[0],
+        players[1],
+    )
 
     return sum(
         (i + 1) * c
-        for i, c in enumerate(deck[::-1])
+        for i, c in enumerate(reversed(deck))
     )
 
 example1 = multiline_lines(r"""
