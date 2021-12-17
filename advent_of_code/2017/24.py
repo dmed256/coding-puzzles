65b213fcb5245c70036a4f5f6772ae3d3afb2bd5
dmed256 <dmed256@gmail.com>
2021-12-16 20:40:11 -0600

    [-O3] 2017.24

--- advent_of_code/2017/24.py
+++ advent_of_code/2017/24.py
@@ -4,36 +4,34 @@ input_value = get_input()
 input_lines = get_input_lines()
 
 def run(problem, lines):
-    tuples = set()
+    matches = defaultdict(set)
     for line in lines:
-        a_, b_ = line.split('/')
-        a_ = int(a_)
-        b_ = int(b_)
-        a = min(a_, b_)
-        b = max(a_, b_)
+        a, b = line.split('/')
+        a = int(a)
+        b = int(b)
 
-        tuples.add((a, b))
+        matches[a].add((a, b))
+        matches[b].add((a, b))
 
     if problem == 1:
         init_value = 0
     else:
         init_value = (0, 0)
 
-    q = [(0, tuples, init_value)]
+    q = [(0, set(), init_value)]
     max_value = init_value
     while q:
-        end, tuples, value = q.pop(0)
+        end, pins, value = q.pop(0)
         max_value = max(max_value, value)
-        for pin in tuples:
-            if problem == 1:
-                pin_value = value + sum(pin)
-            else:
-                pin_value = (value[0] + 1, value[1] + sum(pin))
+        for pin in matches[end] - pins:
+            next_end = sum(pin) - end
 
-            if end == pin[0]:
-                q.append((pin[1], tuples - {pin}, pin_value))
-            elif end == pin[1]:
-                q.append((pin[0], tuples - {pin}, pin_value))
+            if problem == 1:
+                pin_value = value + end + next_end
+            else:
+                pin_value = (value[0] + 1, value[1] + end + next_end)
+
+            q.append((next_end, pins | {pin}, pin_value))
 
     if problem == 1:
         return max_value
