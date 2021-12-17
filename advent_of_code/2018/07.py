1fd5b2cc9eb860ea7b0f277ca84666e9dca479a6
dmed256 <dmed256@gmail.com>
2021-12-17 17:57:14 -0600

    [2018] 07.2

--- advent_of_code/2018/07.py
+++ advent_of_code/2018/07.py
@@ -45,11 +45,55 @@ def run(lines):
 
     return ans
 
-def run2(lines, elves, base_cost):
+def run2(lines, worker_count, base_cost):
     starting_nodes, dependencies, unblocks = parse_lines(lines)
+    workers = [None] * worker_count
 
-    pass
+    node_costs = {
+        node: base_cost + i + 1
+        for i, node in enumerate(string.ascii_uppercase)
+    }
 
+    queue = starting_nodes
+    heapq.heapify(queue)
+
+    time_taken = -1
+    while queue or any(workers):
+        time_taken += 1
+
+        # Decrement counters
+        for wi, info in enumerate(workers):
+            if not info:
+                continue
+
+            node, time_required = info
+            if 1 < time_required:
+                workers[wi] = [node, time_required - 1]
+                continue
+
+            # Finished working on the node
+            workers[wi] = None
+
+            next_nodes = []
+            for enabled_node in unblocks[node]:
+                dependencies[enabled_node] -= {node}
+                if not dependencies[enabled_node]:
+                    next_nodes.append(enabled_node)
+
+            for next_node in sorted(next_nodes):
+                heapq.heappush(queue, next_node)
+
+        for wi, info in enumerate(workers):
+            if info:
+                continue
+
+            if not queue:
+                break
+
+            node = heapq.heappop(queue)
+            workers[wi] = [node, node_costs[node]]
+
+    return time_taken
 
 example1 = multiline_lines(r"""
 Step C must be finished before step A can begin.
@@ -65,6 +109,6 @@ run(example1) | eq('CABDFE')
 
 run(input_lines) | debug('Star 1') | eq('CGKMUWXFAIHSYDNLJQTREOPZBV')
 
-# run2(example1, 2, 0) | eq(15)
+run2(example1, 2, 0) | eq(15)
 
-# run2(input_lines, 5, 60) | submit(2)
+run2(input_lines, 5, 60) | debug('Star 2') | eq(1046)
