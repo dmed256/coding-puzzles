8882d87157a47b48bdb0c2db9520f2826fa769f8
dmed256 <dmed256@gmail.com>
2021-12-21 00:48:18 -0600

    [2018] 23!!!

--- advent_of_code/2018/23.py
+++ advent_of_code/2018/23.py
@@ -4,14 +4,48 @@ input_lines = get_input_lines()
 
 Nanobot = namedtuple('Nanobot', ['pos', 'r'])
 
+
 def in_nanobot(n, pos):
     return pos_distance(n.pos, pos) <= n.r
 
+
+def intersects_bounds(bounds, pos, r):
+    x, y, z = pos
+
+    min_x, max_x = bounds[0]
+    min_y, max_y = bounds[1]
+    min_z, max_z = bounds[2]
+
+    # Find the min-distances to the bounding box
+    # for each axis specifically to get the closest
+    # point in the cube
+    x_dist = max([0, min_x - x, x - max_x])
+    y_dist = max([0, min_y - y, y - max_y])
+    z_dist = max([0, min_z - z, z - max_z])
+
+    return x_dist + y_dist + z_dist <= r
+
+
+def get_bounds_corners(bounds):
+    return list(
+        (x, y, z)
+        for x, y, z in itertools.product(*bounds)
+    )
+
+
+def find_intersections(bounds, nanobots):
+    return sum([
+        intersects_bounds(bounds, n.pos, n.r)
+        for n in nanobots
+    ])
+
+
 def bounds_volume(bounds):
     return mult([
-        bound[1] - bound[2]
-        for bound in bounds
+        bounds[axis][1] - bounds[axis][0]
+        for axis in range(3)
     ])
+
 
 def run(problem, lines):
     nanobots = []
@@ -54,27 +88,66 @@ def run(problem, lines):
     ])
 
     def build_queue_entry(bounds):
-        intersections = find_intersections(bounds)
+        intersections = find_intersections(bounds, nanobots)
         volume = bounds_volume(bounds)
 
-        # TODO: HERE
-        h = ()
+        dist = min([
+            abs(x) + abs(y) + abs(z)
+            for x, y, z in get_bounds_corners(bounds)
+        ])
+
+        # Prioritize counts, then closeness to origin, lastly volume
+        h = (-intersections, -dist, -volume)
 
         return (
             h,
             intersections,
             volume,
+            dist,
             bounds,
         )
 
     queue = [build_queue_entry(max_bounds)]
 
-    while queue:
-        neg_count, vol, bounds = heapq.heappop(queue)
-        max
+    max_intersections = None
+    min_dist = None
 
-        if vol == 1:
-            break
+    while queue:
+        _, intersections, volume, dist, bounds = heapq.heappop(queue)
+
+        if max_intersections:
+            if max_intersections < intersections:
+                break
+            if max_intersections == intersections and min_dist <= dist:
+                break
+
+        if volume == 1:
+            for pos in get_bounds_corners(bounds):
+                pos_dist = pos_distance(pos, (0, 0, 0))
+                pos_intersections = sum([
+                    pos_distance(pos, n.pos) <= n.r
+                    for n in nanobots
+                ])
+
+                if max_intersections is None or max_intersections < pos_intersections:
+                    max_intersections = pos_intersections
+                    min_dist = pos_dist
+                elif max_intersections == pos_intersections:
+                    min_dist = safe_min(min_dist, pos_dist)
+            continue
+
+        mids = [
+            (bounds[axis][0] + bounds[axis][1]) // 2
+            for axis in range(3)
+        ]
+        new_axis_bounds = [
+            [(bounds[axis][0], mids[axis]), (mids[axis], bounds[axis][1])]
+            for axis in range(3)
+        ]
+        for bounds in itertools.product(*new_axis_bounds):
+            heapq.heappush(queue, build_queue_entry(bounds))
+
+    return min_dist
 
 example1 = multiline_lines(r"""
 pos=<0,0,0>, r=4
@@ -88,9 +161,9 @@ pos=<1,1,2>, r=1
 pos=<1,3,1>, r=1
 """)
 
-# run(1, example1) | eq(7)
+run(1, example1) | eq(7)
 
-# run(1, input_lines) | debug('Star 1') | eq(305)
+run(1, input_lines) | debug('Star 1') | eq(305)
 
 example2 = multiline_lines(r"""
 pos=<10,12,12>, r=2
@@ -101,10 +174,6 @@ pos=<50,50,50>, r=200
 pos=<10,10,10>, r=5
 """)
 
-# run(2, example2) | eq(36)
+run(2, example2) | eq(36)
 
-# High: 82444725
-# High: 82444619
-# High: 82444690
-#       78687692
 run(2, input_lines) | debug('Star 2')
