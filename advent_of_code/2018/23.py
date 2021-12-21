125b655247fb92bc11bd072a9a337edcc02169cd
dmed256 <dmed256@gmail.com>
2021-12-20 18:19:55 -0600

    [2018] 23.2 (temp)

--- advent_of_code/2018/23.py
+++ advent_of_code/2018/23.py
@@ -14,50 +14,181 @@ def get_largest_groups(nanobots):
                 links[ni1].add(ni2)
                 links[ni2].add(ni1)
 
-    def get_group(p):
-        queue = [p]
-        group = {p}
-        seen = set()
-        while queue:
-            p1 = queue.pop()
-
-            if p1 in seen:
+    def get_largest_groups_for(p1, pairs_visited):
+        visited = {p1}
+        max_groups = [[p1]]
+        for p2 in links[p1]:
+            if p2 == p1 or p2 in visited:
                 continue
-            seen.add(p1)
 
-            if group <= links[p1]:
-                group.add(p1)
+            if (p1, p2) in pairs_visited:
+                continue
 
-            queue += links[p1] - group
+            pairs_visited.add((p1, p2))
+            pairs_visited.add((p2, p1))
 
-        return group
+            visited.add(p2)
 
-    max_groups = []
-    max_group_size = 0
+            group = [p1, p2]
+            while True:
+                nodes = set(links[group[0]])
+                for point in group[1:]:
+                    nodes &= links[point]
+                nodes -= visited
 
-    visited = set()
-    for ni in range(len(nanobots)):
-        if ni in visited:
-            continue
+                if not nodes:
+                    break
 
-        group = get_group(ni)
-        visited |= group
+                next_point = min(nodes)
 
-        if max_group_size < len(group):
-            max_groups = [group]
-            max_group_size = len(group)
-        elif max_group_size == len(group):
+                if (next_point, p1) in pairs_visited:
+                    break
+
+                for point in group:
+                    pairs_visited.add((point, next_point))
+                    pairs_visited.add((next_point, point))
+
+                visited.add(next_point)
+                group.append(next_point)
+
+            if len(group) < len(max_groups[0]):
+                continue
+
+            if len(group) > len(max_groups[0]):
+                max_groups = [group]
+                continue
+
             max_groups.append(group)
 
-    return max_groups
+        return max_groups
 
+    pairs_visited = set()
+    groups = [
+        group
+        for ni in range(len(nanobots))
+        for group in get_largest_groups_for(ni, pairs_visited)
+    ]
+
+    max_length = max([
+        len(group)
+        for group in groups
+    ])
+
+    return {
+        tuple(sorted(group))
+        for group in groups
+        if len(group) == max_length
+    }
+
+# Find midpoint between the circles and average them all up
+# Circles should be convex so we just need to traverse toward (0, 0, 0)
 def get_min_intersection(nanobots, group):
-    # ???
-    # Find x ranges
-    # Filter y ranges
-    # Filter z ranges
-    # Get min value
-    pass
+    group = set(group)
+    print(group)
+
+    # If a circle A completely covers another circle B, there is no
+    # need to check whether a point is in A if it's in B
+    covers = set()
+    for ni1 in range(len(group)):
+        n1 = nanobots[ni1]
+        for ni2 in range(ni1 + 1, len(group)):
+            n2 = nanobots[ni2]
+
+            if pos_distance(n1.pos, n2.pos) + n2.r <= n1.r:
+                covers.add(ni1)
+                break
+
+    group -= covers
+
+    def get_midpoint(n1, n2, axis):
+        _, p1, p2, _ = sorted([
+            n1.pos[axis] + n1.r,
+            n1.pos[axis] - n1.r,
+            n2.pos[axis] + n2.r,
+            n2.pos[axis] - n2.r,
+        ])
+        return (p1 + p2) / 2
+
+    center_points = [[], [], []]
+    for ni1 in range(len(group)):
+        n1 = nanobots[ni1]
+        for ni2 in range(ni1 + 1, len(group)):
+            n2 = nanobots[ni2]
+
+            for axis in range(3):
+                center_points[axis].append(
+                    get_midpoint(n1, n2, axis)
+                )
+
+    center = [
+        sum(point[axis] for point in center_points) / len(center_points)
+        for axis in range(3)
+    ]
+
+    # Find grid positions around the probably-not-grid-position center
+    grid_centers = [
+        (x, y, z)
+        for x in {math.floor(center[0]), math.ceil(center[0])}
+        for y in {math.floor(center[1]), math.ceil(center[1])}
+        for z in {math.floor(center[2]), math.ceil(center[2])}
+    ]
+
+    for pos in grid_centers:
+        print(sorted([
+            max(0, pos_distance(n.pos, pos) - n.r)
+            for ni in group
+            if (n := nanobots[ni])
+        ]))
+
+    def in_group(pos):
+        return all(
+            pos_distance(n.pos, pos) <= n.r
+            for ni in group
+            if (n := nanobots[ni])
+        )
+
+    # Make sure the grid points are inside all groups since we're
+    # approximating
+    grid_centers = [
+        grid_center
+        for grid_center in grid_centers
+        if in_group(grid_center)
+    ]
+
+    origin = (0, 0, 0)
+    def get_distance(pos):
+        return pos_distance(pos, origin)
+
+    visited = set()
+    queue = heapify([
+        (get_distance(pos), pos)
+        for pos in grid_centers
+    ])
+    min_dist = None
+
+    while queue:
+        dist, pos = heapq.heappop(queue)
+
+        if pos in visited:
+            continue
+        visited.add(pos)
+
+        if min_dist and min_dist <= dist:
+            continue
+        min_dist = dist
+
+        for direction in DIRECTIONS:
+            npos = apply_direction(pos, direction)
+
+            if not in_group(npos):
+                continue
+
+            ndist = get_distance(npos)
+            if ndist < dist:
+                heapq.heappush(queue, (ndist, npos))
+
+    return min_dist
+
 
 def run(problem, lines):
     nanobots = []
@@ -75,6 +206,8 @@ def run(problem, lines):
             for n2 in nanobots
             if pos_distance(largest_nanobot.pos, n2.pos) <= largest_nanobot.r
         ])
+
+    print(get_largest_groups(nanobots))
 
     return min(
         get_min_intersection(nanobots, group)
@@ -107,6 +240,6 @@ pos=<50,50,50>, r=200
 pos=<10,10,10>, r=5
 """)
 
-run(2, example2) | eq(36)
+# run(2, example2) | eq(36)
 
-run(2, input_lines) | debug('Star 2')
+# run(2, input_lines) | debug('Star 2')
